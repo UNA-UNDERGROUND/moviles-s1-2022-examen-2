30001b9a91f7a2e6b7ad6768729d212e07070955
Jonathan Guzmán <jonguz5@hotmail.com>
2022-06-24 21:17:34 -0600

    model

---
+++ web/model/activity.php
@@ -0,0 +1,160 @@
+<?php
+
+
+class Activity implements JsonSerializable
+{
+    private int $id;
+    private int $idTrainingPlan;
+    private string $day;
+    private string $name;
+    private string $repetitions;
+    private string $breaks;
+    private string $series;
+    private string $cadence;
+    private string $weight;
+
+    public function __construct(
+        int $id,
+        int $idTrainingPlan,
+        string $day,
+        string $name,
+        string $repetitions,
+        string $breaks,
+        string $series,
+        string $cadence,
+        string $weight
+    ) {
+        $this->id = $id;
+        $this->idTrainingPlan = $idTrainingPlan;
+        $this->day = $day;
+        $this->name = $name;
+        $this->repetitions = $repetitions;
+        $this->breaks = $breaks;
+        $this->series = $series;
+        $this->cadence = $cadence;
+        $this->weight = $weight;
+    }
+
+    // getters
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getIdTrainingPlan(): int
+    {
+        return $this->idTrainingPlan;
+    }
+
+    public function getDay(): string
+    {
+        return $this->day;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getRepetitions(): string
+    {
+        return $this->repetitions;
+    }
+
+    public function getBreaks(): string
+    {
+        return $this->breaks;
+    }
+
+    public function getSeries(): string
+    {
+        return $this->series;
+    }
+
+    public function getCadence(): string
+    {
+        return $this->cadence;
+    }
+
+    public function getWeight(): string
+    {
+        return $this->weight;
+    }
+
+    // setters
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setIdTrainingPlan(int $idTrainingPlan): void
+    {
+        $this->idTrainingPlan = $idTrainingPlan;
+    }
+
+    public function setDay(string $day): void
+    {
+        $this->day = $day;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setRepetitions(string $repetitions): void
+    {
+        $this->repetitions = $repetitions;
+    }
+
+    public function setBreaks(string $breaks): void
+    {
+        $this->breaks = $breaks;
+    }
+
+    public function setSeries(string $series): void
+    {
+        $this->series = $series;
+    }
+
+    public function setCadence(string $cadence): void
+    {
+        $this->cadence = $cadence;
+    }
+
+    public function setWeight(string $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            "id" => $this->id,
+            "day" => $this->day,
+            "name" => $this->name,
+            "repetitions" => $this->repetitions,
+            "breaks" => $this->breaks,
+            "series" => $this->series,
+            "cadence" => $this->cadence,
+            "weight" => $this->weight,
+        ];
+    }
+
+    public static function fromJson(array $json): Activity
+    {
+        return new Activity(
+            $json["id"],
+            $json["idTrainingPlan"],
+            $json["day"],
+            $json["name"],
+            $json["repetitions"],
+            $json["breaks"],
+            $json["series"],
+            $json["cadence"],
+            $json["weight"]
+        );
+    }
+};
--- web/model/trainingPlan.php
+++ web/model/trainingPlan.php
@@ -1,16 +1,86 @@
 <?php
+// Activity class
+require_once('activity.php');
 
-
-class TrainingPlan implements JsonSerializable{
-
+class TrainingPlan implements JsonSerializable
+{
     private int $id;
+    private string $username;
+    private string $name;
+    // array of activities
+    private array $activities;
 
-    public TrainingPlan(){
-        
+    public function __construct(int $id, string $username, string $name, array $activities)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->name = $name;
+        $this->activities = $activities;
+    }
+
+    // getters
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getActivities(): array
+    {
+        return $this->activities;
+    }
+
+    // setters
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setActivities(array $activities): void
+    {
+        $this->activities = $activities;
+    }
+
+    public static function fromJson(array $json): TrainingPlan
+    {
+        $id = $json['id'];
+        $username = $json['username'];
+        $name = $json['name'];
+        $activities = [];
+        foreach ($json['activities'] as $activity) {
+            $activities[] = Activity::fromJson($activity);
+        }
+        return new TrainingPlan($id, $username, $name, $activities);
     }
 
     public function jsonSerialize(): mixed
     {
-        return get_object_vars($this);
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'name' => $this->name,
+            'activities' => $this->activities,
+        ];
     }
 };
