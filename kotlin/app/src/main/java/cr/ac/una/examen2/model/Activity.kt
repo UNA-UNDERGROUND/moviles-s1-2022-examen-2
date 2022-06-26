4038e6cbff9357d9a29dccc36507028d10c91d67
Jonathan Guzmán <jonguz5@hotmail.com>
2022-06-25 22:58:47 -0600

    model

---
+++ kotlin/app/src/main/java/cr/ac/una/examen2/model/Activity.kt
@@ -0,0 +1,148 @@
+package cr.ac.una.examen2.model
+
+import org.json.JSONObject
+import java.util.*
+
+
+class Activity {
+    var id: Int? = null
+    var idTrainingPlan: Int? = null
+    var day: Day
+    var name: String
+    var repetitions: Int
+    var breaks: Int
+    var series: Int
+    var cadence: Int
+    var weight: Int
+
+    constructor(
+        id: Int?,
+        idTrainingPlan: Int?,
+        day: Day,
+        name: String,
+        repetitions: Int,
+        breaks: Int,
+        series: Int,
+        cadence: Int,
+        weight: Int
+    ) {
+        this.id = id
+        this.idTrainingPlan = idTrainingPlan
+        this.day = day
+        this.name = name
+        this.repetitions = repetitions
+        this.breaks = breaks
+        this.series = series
+        this.cadence = cadence
+        this.weight = weight
+        validate()
+    }
+
+
+    // enum class for day
+    // standing a id code and a char code
+    enum class Day(var id: Int, var char: Char) {
+        MONDAY(1, 'S'),
+        TUESDAY(2, 'T'),
+        WEDNESDAY(3, 'W'),
+        THURSDAY(4, 'R'),
+        FRIDAY(5, 'F'),
+        SATURDAY(6, 'S'),
+        SUNDAY(7, 'U');
+
+        companion object {
+            fun getDayById(id: Int): Day {
+                for (day in values()) {
+                    if (day.id == id) {
+                        return day
+                    }
+                }
+                // if not throw exception
+                throw IllegalArgumentException("Invalid day id: $id")
+            }
+            fun getDayByChar(char: String): Day {
+                val dayChr = char.uppercase(Locale.ROOT)[0].toString()[0]
+                for (day in values()) {
+                    if (day.char == dayChr) {
+                        return day
+                    }
+                }
+                // if not throw exception
+                throw IllegalArgumentException("Invalid day char: $char")
+            }
+        }
+    }
+
+    // from JSON to Activity
+    constructor(jsonObject: JSONObject) {
+        // id may not be present
+        if (jsonObject.has("id")) {
+            id = jsonObject.getInt("id")
+        }
+        // idTrainingPlan may not be present
+        if (jsonObject.has("idTrainingPlan")) {
+            idTrainingPlan = jsonObject.getInt("idTrainingPlan")
+        }
+        // day is a string, so we need to convert it to a Day enum
+        val day = jsonObject["day"] as String
+        this.day = Day.getDayByChar(day)
+        name = jsonObject["name"] as String
+        repetitions = jsonObject["repetitions"] as Int
+        breaks = jsonObject["breaks"] as Int
+        series = jsonObject["series"] as Int
+        cadence = jsonObject["cadence"] as Int
+        weight = jsonObject["weight"] as Int
+        validate()
+    }
+
+    // from Activity to JSON
+    fun toJSON(): JSONObject {
+        val jsonObject = JSONObject()
+        // id may not be present
+        if (id != null) {
+            jsonObject.put("id", id)
+        }
+        // idTrainingPlan may not be present
+        if (idTrainingPlan != null) {
+            jsonObject.put("idTrainingPlan", idTrainingPlan)
+        }
+        // we need to get the char and convert it to a string
+        val dayChar = day.char.toString()
+        jsonObject.put("day", dayChar)
+        jsonObject.put("name", name)
+        jsonObject.put("repetitions", repetitions)
+        jsonObject.put("breaks", breaks)
+        jsonObject.put("series", series)
+        jsonObject.put("cadence", cadence)
+        jsonObject.put("weight", weight)
+        return jsonObject
+    }
+
+    private fun validate() {
+        // name cannot be blank
+        if (name.isBlank()) {
+            throw IllegalArgumentException("Name cannot be blank")
+        }
+        // repetitions must be greater than 0
+        if (repetitions <= 0) {
+            throw IllegalArgumentException("Repetitions must be greater than 0")
+        }
+        // breaks must not be negative
+        if (breaks < 0) {
+            throw IllegalArgumentException("Breaks must not be negative")
+        }
+        // series must not be negative
+        if (series < 0) {
+            throw IllegalArgumentException("Series must not be negative")
+        }
+        // cadence must not be negative
+        if (cadence < 0) {
+            throw IllegalArgumentException("Cadence must not be negative")
+        }
+        // weight must not be negative
+        if (weight < 0) {
+            throw IllegalArgumentException("Weight must not be negative")
+        }
+    }
+
+}
---
+++ kotlin/app/src/main/java/cr/ac/una/examen2/model/TrainingPlan.kt
@@ -0,0 +1,71 @@
+package cr.ac.una.examen2.model
+
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+class TrainingPlan {
+    var id: Int? = null
+    var name: String
+    var username: String
+    var activities: List<Activity>? = null
+
+    constructor(id: Int?, name: String, username: String, activities: List<Activity>) {
+        this.id = id
+        this.name = name
+        this.username = username
+        this.activities = activities
+        validate()
+    }
+
+    constructor(jsonObject: JSONObject){
+        // id may be null
+        if (jsonObject.has("id")) {
+            this.id = jsonObject.getInt("id")
+        }
+        this.name = jsonObject.getString("name")
+        this.username = jsonObject.getString("username")
+        // activities be may null
+        if (jsonObject.has("activities")) {
+            val activitiesJsonArray = jsonObject.getJSONArray("activities")
+            val activities = ArrayList<Activity>()
+            for (i in 0 until activitiesJsonArray.length()) {
+                activities.add(Activity(activitiesJsonArray.getJSONObject(i)))
+            }
+            this.activities = activities
+        }
+        validate()
+    }
+
+    private fun validate(){
+        // name cannot be blank
+        if(name.isBlank()){
+            throw IllegalArgumentException("name cannot be blank")
+        }
+        // username cannot be blank
+        if(username.isBlank()){
+            throw IllegalArgumentException("username cannot be blank")
+        }
+    }
+
+    private fun toJSON(): JSONObject {
+        val jsonObject = JSONObject()
+        if (id != null) {
+            jsonObject.put("id", id)
+        }
+        jsonObject.put("name", name)
+        jsonObject.put("username", username)
+        if (activities != null) {
+            val activitiesJsonArray = JSONArray()
+            for (activity in activities!!) {
+                activitiesJsonArray.put(activity.toJSON())
+            }
+            jsonObject.put("activities", activitiesJsonArray)
+        }
+        return jsonObject
+    }
+
+    override fun toString(): String {
+        return toJSON().toString()
+    }
+}
